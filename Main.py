e6f4472d3cec16737e664353602e51032a7d484b
Beyarkay <boydrkane@gmail.com>
2018-03-01 18:03:06 +0100

    Fixing a bit of a read_file method together

--- Main.py
+++ Main.py
@@ -4,7 +4,14 @@ def read_file(filename):
     several_more_variables = map(int, f.readline().strip().split())
 
 def main():
-    pass
+    files = ["example.in", "easy.in", "medium.in", "hard.in", ]
+    read_file(files[0])
+
+
+    # # For testing all the files
+    # for file in files:
+    #     read_file(file)
+
 
 if __name__ == '__main__':
     main()
