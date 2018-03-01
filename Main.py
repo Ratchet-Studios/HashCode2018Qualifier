2583933e94306b05987c20d1e0f7d6a70ce1e509
Luc Hayward <luchay10@gmail.com>
2018-03-01 20:02:32 +0200

    started inputs

--- Main.py
+++ Main.py
@@ -1,7 +1,8 @@
+Rows, Columns, num_vehicles, num_rides, ride_bonus, num_steps = 0,0,0,0,0,0
 def read_file(filename):
     f = open(filename)
     # take the first line, strip extra whitespace, convert to an array by spaces, convert each element to an int
-    var1, var2, var3, etc = map(int, f.readline().strip().split())
+    global Rows, Columns, num_vehicles, num_rides, ride_bonus, num_steps = map(int, f.readline().strip().split())
     var4, var5, var6, etc = map(int, f.readline().strip().split())
 
 
@@ -14,6 +15,12 @@ def main():
     # for file in files:
     #     read_file(file)
 
+
+
+
+def is_valid_file(submission_array):
+    if len(submission_array) != num_rides:
+        return 0
 
 if __name__ == '__main__':
     main()
