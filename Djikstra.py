b35b140f147ae851fb396a1779f2ca390fc8ee51
Luc Hayward <luchay10@gmail.com>
2018-03-01 19:48:36 +0200

    two djikstra versions

--- Djikstra.py
+++ Djikstra.py
@@ -1,43 +1,80 @@
+# python 3 only!!!
+import collections
+import math
+
+
 class Graph:
-  def __init__(self):
-    self.nodes = set()
-    self.edges = defaultdict(list)
-    self.distances = {}
+    ''' graph class inspired by https://gist.github.com/econchick/4666413
+    '''
+    
+    def __init__(self):
+        self.vertices = set()
+        
+        # makes the default value for all vertices an empty list
+        self.edges = collections.defaultdict(list)
+        self.weights = {}
+    
+    def add_vertex(self, value):
+        self.vertices.add(value)
+    
+    def add_edge(self, from_vertex, to_vertex, distance):
+        if from_vertex == to_vertex: pass  # no cycles allowed
+        self.edges[from_vertex].append(to_vertex)
+        self.weights[(from_vertex, to_vertex)] = distance
+    
+    def __str__(self):
+        string = "Vertices: " + str(self.vertices) + "\n"
+        string += "Edges: " + str(self.edges) + "\n"
+        string += "Weights: " + str(self.weights)
+        return string
 
-  def add_node(self, value):
-    self.nodes.add(value)
 
-  def add_edge(self, from_node, to_node, distance):
-    self.edges[from_node].append(to_node)
-    self.edges[to_node].append(from_node)
-    self.distances[(from_node, to_node)] = distance
+def dijkstra(graph, start):
+    # initializations
+    S = set()
+    
+    # delta represents the length shortest distance paths from start -> v, for v in delta.
+    # We initialize it so that every vertex has a path of infinity (this line will break if you run python 2)
+    delta = dict.fromkeys(list(graph.vertices), math.inf)
+    previous = dict.fromkeys(list(graph.vertices), None)
+    
+    # then we set the path length of the start vertex to 0
+    delta[start] = 0
+    
+    # while there exists a vertex v not in S
+    while S != graph.vertices:
+        # let v be the closest vertex that has not been visited...it will begin at 'start'
+        v = min((set(delta.keys()) - S), key=delta.get)
+        
+        # for each neighbor of v not in S
+        for neighbor in set(graph.edges[v]) - S:
+            new_path = delta[v] + graph.weights[v, neighbor]
+            
+            # is the new path from neighbor through
+            if new_path < delta[neighbor]:
+                # since it's optimal, update the shortest path for neighbor
+                delta[neighbor] = new_path
+                
+                # set the previous vertex of neighbor to v
+                previous[neighbor] = v
+        S.add(v)
+    
+    return (delta, previous)
 
 
-def dijsktra(graph, initial):
-  visited = {initial: 0}
-  path = {}
+def shortest_path(graph, start, end):
+    '''Uses dijkstra function in order to output the shortest path from start to end
+    '''
 
-  nodes = set(graph.nodes)
 
-  while nodes:
-    min_node = None
-    for node in nodes:
-      if node in visited:
-        if min_node is None:
-          min_node = node
-        elif visited[node] < visited[min_node]:
-          min_node = node
+delta, previous = dijkstra(graph, start)
 
-    if min_node is None:
-      break
+path = []
+vertex = end
 
-    nodes.remove(min_node)
-    current_weight = visited[min_node]
+while vertex is not None:
+    path.append(vertex)
+    vertex = previous[vertex]
 
-    for edge in graph.edges[min_node]:
-      weight = current_weight + graph.distance[(min_node, edge)]
-      if edge not in visited or weight < visited[edge]:
-        visited[edge] = weight
-        path[edge] = min_node
-
-  return visited, path
+path.reverse()
+return path
